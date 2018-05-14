cc9081d51e89ff2d8714fcdb46cd8e67d61dbb8c
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-14 19:33:59 +0300

    atkartojums

--- day_7/day_7/Program.cs
+++ day_7/day_7/Program.cs
@@ -6,8 +6,34 @@ namespace day_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReizTabula();
+
+            /*  FOR FUNKCIJAS PIEMERS
+             **/ 
+           
+
+            /*
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine(i + "*" + skaitlis + "=" + i * skaitlis);
+                
+            }
+            */
             Console.ReadLine();
         }
+
+        static void ReizTabula()
+        {
+            Console.WriteLine("Ievadiet skaitli: ");
+            string ievade = Console.ReadLine();
+            int skaitlis = Convert.ToInt16(ievade);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + "*" + skaitlis + "=" + i * skaitlis);
+            }
+
+        }
     }
+
 }
